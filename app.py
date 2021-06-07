4398ce036b1fcc4552c4370a671b44ee7f16e4cf
정훈박 <fireking5997@gmail.com>
2021-06-07 14:07:49 +0900

    abc

--- app.py
+++ app.py
@@ -6,14 +6,12 @@ from bs4 import BeautifulSoup
 
 client = MongoClient('localhost', 27017)
 app = Flask(__name__)
-db = client.DBNAME
+db = client.sparataWeb1
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
-
-data = {"name":"a"}
 
 @app.route("/main")
 def home():
