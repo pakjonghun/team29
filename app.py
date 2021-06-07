9687d444f5160441618253799984b489ed84cc67
minju <minjuice93@gmail.com>
2021-06-07 18:04:35 +0900

    main.html 대략적인 틀 완성

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask,render_template
 
 from pymongo import MongoClient
-import requests
 from bs4 import BeautifulSoup
+import requests
+
 
 client = MongoClient('localhost', 27017)
 app = Flask(__name__)
-db = client.sparataWeb1
+db = client.sparataWeb
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
